881c684f6a712236c89d59fb74adba6f5cf0cc53
Guilherme <guilhermedias2501@gmail.com>
2024-09-09 15:26:42 -0300

    Upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é um dos maiores fenômenos do skate mundial. Com apenas alguns anos, ela já conquistou o mundo com sua habilidade e carisma. A mais jovem medalhista olímpica brasileira, Rayssa é um exemplo de superação e talento.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8",
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica ouro"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma das maiores promessas do judô brasileiro. Sua técnica apurada e determinação a colocam entre as principais atletas da modalidade. Com uma trajetória de vitórias e superação, Beatriz inspira jovens atletas em todo o país.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoka"
+    },
+]
